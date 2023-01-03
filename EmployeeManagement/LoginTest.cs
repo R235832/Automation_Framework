132c9a8e2e9c6eef6c0c63cb2608d0df222b1ebf
rakeshro <rakeshroutjspofficial@gmail.com>
2023-01-03 17:40:47 +0530

    Assignment

---
+++ EmployeeManagement/LoginTest.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Base
+
+{
+    public class LoginTest : AutomationWrapper
+    {
+        [Test]
+        public void ValidLoginTest()
+        {
+            driver.FindElement(By.Name("username")).SendKeys("Admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin123");
+            driver.FindElement(By.XPath(" //button[normalize-space()=\"Login\"]")).Click();
+            string homePageLink = driver.Url;
+            Assert.That(homePageLink, Is.EqualTo("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"));
+        }
+    }
+}
