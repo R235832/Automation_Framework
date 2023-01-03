94c536e979efe677387fff47aaf573dc99e1ea13
rakeshro <rakeshroutjspofficial@gmail.com>
2023-01-03 16:44:21 +0530

    OrangeHRM LoginPage

--- EmployeeManagement/LoginUiTest.cs
+++ EmployeeManagement/LoginUiTest.cs
@@ -5,16 +5,43 @@ namespace EmployeeManagement
 {
     public class LoginUiTest
     {
+        IWebDriver driver;
+        [SetUp]
+        public void AfterMethod()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+        }
+        [TearDown]
+        public void BeforeMethod()
+        {
+            driver.Quit();
+        }
+
         [Test]
+
         public void ValidateTitleTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(30);
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+
             string actualtitle=driver.Title;
             Assert.That(actualtitle,Is.EqualTo("OrangeHRM"));
-           // Console.WriteLine(actualtitle); 
+           
+        }
+
+        [Test]
+        public void ValidatePlaceholderTest()
+        {
+             
+           
+            string actualUsernameResult= driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            string actualPasswordResult = driver.FindElement(By.Name("password")).GetAttribute("placeholder");
+            Assert.That(actualUsernameResult, Is.EqualTo("Username"));
+            Assert.That(actualPasswordResult, Is.EqualTo("Password"));
+
+
+
         }
     }
 }
